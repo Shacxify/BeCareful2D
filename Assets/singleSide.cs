ce76df217bc58f9a8dd42e97566492bde27fdbe6
Cash Johnson <me@cashjohnson.net>
2017-06-18 10:43:56 -0700

    SETUP v1.0.0
    Just environment

---
+++ Assets/singleSide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singleSide : MonoBehaviour {
+
+	float speed;
+	// Use this for initialization
+	void Start () {
+		speed = GameObject.Find("Main Camera").GetComponent<mainBrain>().speed * -1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
