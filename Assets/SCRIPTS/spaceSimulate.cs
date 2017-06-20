f7a71a5f675587026b5eb3f54b3937e0fc45b15e
Cash Johnson <me@cashjohnson.net>
2017-06-19 22:42:10 -0700

    v1.0.3

---
+++ Assets/SCRIPTS/spaceSimulate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceSimulate : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float moveSpeed;
+
+	void Start () {
+		moveSpeed = Random.Range(0.001f,0.002f);
+	}
+
+	void FixedUpdate () {
+		rb.velocity = new Vector2(0,0);
+    rb.angularVelocity = 0f;
+		rb.AddForce(new Vector2(moveSpeed,moveSpeed));
+	}
+}
